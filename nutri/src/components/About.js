952c00889e1cbf956292da561d65e789b8135220
Frédéric.Toppan <toppan.frederic@gmail.com>
2024-09-17 12:31:23 +0200

    test color stage 2

--- nutri/src/components/About.js
+++ nutri/src/components/About.js
@@ -4,7 +4,7 @@ import portrait from '../images/20240719_102243.jpg';
 const About = () => {
     return (
         <div  className='about'>
-            <img alt='Vivi' src={portrait}></img>
+            <img alt='Vivi' src={portrait} className='portrait'></img>
         </div>
     ); 
 }
